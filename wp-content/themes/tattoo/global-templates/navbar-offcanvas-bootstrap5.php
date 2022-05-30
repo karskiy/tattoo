1c65ac84e45e643c28179c3504ae38882ee7d4e2
Evgenius <dev@omsk.dev>
2022-05-30 14:28:07 +0300

    + navbar-offcanvas-bootstrap5.php

--- wp-content/themes/tattoo/global-templates/navbar-offcanvas-bootstrap5.php
+++ wp-content/themes/tattoo/global-templates/navbar-offcanvas-bootstrap5.php
@@ -40,25 +40,32 @@ $container = get_theme_mod( 'understrap_container_type' );
 		?>
 		<!-- end custom logo -->
 
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+		<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNavOffcanvas" aria-controls="navbarNavOffcanvas" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
-		<!-- The WordPress Menu goes here -->
-		<?php
-		wp_nav_menu(
-			array(
-				'theme_location'  => 'primary',
-				'container_class' => 'collapse navbar-collapse',
-				'container_id'    => 'navbarNavDropdown',
-				'menu_class'      => 'navbar-nav ms-auto',
-				'fallback_cb'     => '',
-				'menu_id'         => 'main-menu',
-				'depth'           => 2,
-				'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-			)
-		);
-		?>
+		<div class="offcanvas offcanvas-end bg-primary" tabindex="-1" id="navbarNavOffcanvas">
+
+			<div class="offcanvas-header justify-content-end">
+				<button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+			</div><!-- .offcancas-header -->
+
+			<!-- The WordPress Menu goes here -->
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location'  => 'primary',
+					'container_class' => 'offcanvas-body',
+					'container_id'    => '',
+					'menu_class'      => 'navbar-nav justify-content-between flex-grow-1 pe-3',
+					'fallback_cb'     => '',
+					'menu_id'         => 'main-menu',
+					'depth'           => 2,
+					'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+				)
+			);
+			?>
+		</div><!-- .offcanvas -->
 
 	</div><!-- .container(-fluid) -->
 
